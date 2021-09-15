cdabcd636b70a47adf8c06e9dde4f34c46b68a5d
pclucas14 <lucas.page-caccia@mail.mcgill.ca>
2021-09-15 16:20:26 +0000

    conv model

--- pytorch/networks/Networks.py
+++ pytorch/networks/Networks.py
@@ -11,6 +11,89 @@ import torch.nn.init as init
 from torch import nn
 from torch.nn import functional as F
 from networks.Layers import *
+from collections import OrderedDict as OD
+
+# From Somewhere else
+class Encoder(nn.Module):
+    def __init__(self, channels, output_size):
+        super(Encoder, self).__init__()
+        self.encoder = nn.Sequential(
+            nn.Conv2d(1, channels, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(channels),
+            nn.ReLU(True),
+            nn.Conv2d(channels, channels, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(channels),
+            Residual(channels),
+            Residual(channels, ds=True),
+            nn.Conv2d(channels, output_size, 1)
+        )
+
+    def forward(self, x):
+        return self.encoder(x)
+
+
+class Decoder(nn.Module):
+    def __init__(self, channels, input_size):
+        super(Decoder, self).__init__()
+        self.decoder = nn.Sequential(
+            nn.Conv2d(input_size, channels, 1, bias=False),
+            nn.BatchNorm2d(channels),
+            Residual(channels, us=True),
+            Residual(channels),
+            nn.ConvTranspose2d(channels, channels, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(channels),
+            nn.ReLU(True),
+            nn.ConvTranspose2d(channels, channels, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(channels),
+            nn.ReLU(True),
+            nn.Conv2d(channels, 1, 1) #3 * 256, 1)
+        )
+
+    def forward(self, x):
+        x = self.decoder(x)
+        B, _, H, W = x.size()
+        return x
+
+
+class Residual(nn.Module):
+    def __init__(self, channels, us=False, ds=False):
+        super(Residual, self).__init__()
+        self.us = us
+        self.ds = ds
+
+        layers = [
+            nn.ReLU(True),
+            nn.Conv2d(channels, channels, 3, 1, 1, bias=False),
+            nn.BatchNorm2d(channels),
+            nn.ReLU(True)
+        ]
+        if ds:
+            layers += [
+                nn.Conv2d(channels, channels, 1, bias=False, stride=2),
+                nn.BatchNorm2d(channels)
+            ]
+        elif us:
+            layers += [
+                nn.ConvTranspose2d(channels, channels, 1, bias=False, stride=2),
+                nn.BatchNorm2d(channels)
+            ]
+        else:
+            layers += [
+                nn.Conv2d(channels, channels, 1, bias=False),
+                nn.BatchNorm2d(channels)
+            ]
+
+        self.block = nn.Sequential(*layers)
+
+
+    def forward(self, x):
+        input = x
+        out = self.block(x)
+        if self.us or self.ds:
+            input = F.upsample(x, size=out.shape[2:])
+
+        return input + out
+
 
 # Inference Network
 class InferenceNet(nn.Module):
@@ -18,8 +101,24 @@ class InferenceNet(nn.Module):
     super(InferenceNet, self).__init__()
 
     # q(y|x)
+    '''
     self.inference_qyx = torch.nn.ModuleList([
         nn.Linear(x_dim, 512),
+        nn.ReLU(),
+        nn.Linear(512, 512),
+        nn.ReLU(),
+        GumbelSoftmax(512, y_dim)
+    ])
+    '''
+
+    self.trunk = nn.Sequential(
+            Encoder(128, 16),
+            nn.Flatten(1)
+    )
+
+
+    self.inference_qyx = torch.nn.ModuleList([
+        nn.Linear(16 * 4 * 4, 512),
         nn.ReLU(),
         nn.Linear(512, 512),
         nn.ReLU(),
@@ -28,7 +127,7 @@ class InferenceNet(nn.Module):
 
     # q(z|y,x)
     self.inference_qzyx = torch.nn.ModuleList([
-        nn.Linear(x_dim + y_dim, 512),
+        nn.Linear(16 * 4 * 4 + y_dim, 512),
         nn.ReLU(),
         nn.Linear(512, 512),
         nn.ReLU(),
@@ -48,24 +147,36 @@ class InferenceNet(nn.Module):
 
   # q(z|x,y)
   def qzxy(self, x, y):
-    concat = torch.cat((x, y), dim=1)  
+    concat = torch.cat((x, y), dim=1)
     for layer in self.inference_qzyx:
       concat = layer(concat)
     return concat
-  
+
   def forward(self, x, temperature=1.0, hard=0):
+    if x.ndim == 2:
+        x = x.reshape(-1, 1, 28, 28)
+
     #x = Flatten(x)
+    raw_x = x
+    x = self.trunk(x)
 
     # q(y|x)
     logits, prob, y = self.qyx(x, temperature, hard)
-    
+
     # q(z|x,y)
     mu, var, z = self.qzxy(x, y)
 
-    output = {'mean': mu, 'var': var, 'gaussian': z, 
+    output = {'mean': mu, 'var': var, 'gaussian': z,
               'logits': logits, 'prob_cat': prob, 'categorical': y}
     return output
 
+class View(nn.Module):
+    def __init__(self, shape):
+        super().__init__()
+        self.shape = shape
+
+    def forward(self, x):
+        return x.view(*self.shape)
 
 # Generative Network
 class GenerativeNet(nn.Module):
@@ -77,6 +188,7 @@ class GenerativeNet(nn.Module):
     self.y_var = nn.Linear(y_dim, z_dim)
 
     # p(x|z)
+    '''
     self.generative_pxz = torch.nn.ModuleList([
         nn.Linear(z_dim, 512),
         nn.ReLU(),
@@ -85,23 +197,30 @@ class GenerativeNet(nn.Module):
         nn.Linear(512, x_dim),
         torch.nn.Sigmoid()
     ])
+    '''
+    self.generative_pxz = nn.Sequential(
+        nn.Linear(z_dim, 4 * 4 * 16),
+        View((-1, 16, 4, 4)),
+        Decoder(128, 16),
+        nn.Sigmoid(),
+        nn.Flatten(1)
+    )
+
 
   # p(z|y)
   def pzy(self, y):
     y_mu = self.y_mu(y)
     y_var = F.softplus(self.y_var(y))
     return y_mu, y_var
-  
+
   # p(x|z)
   def pxz(self, z):
-    for layer in self.generative_pxz:
-      z = layer(z)
-    return z
+    return self.generative_pxz(z)
 
   def forward(self, z, y):
     # p(z|y)
     y_mu, y_var = self.pzy(y)
-    
+
     # p(x|z)
     x_rec = self.pxz(z)
 
@@ -119,17 +238,20 @@ class GMVAENet(nn.Module):
 
     # weight initialization
     for m in self.modules():
+      if m._modules is None:
+          continue
+
       if type(m) == nn.Linear or type(m) == nn.Conv2d or type(m) == nn.ConvTranspose2d:
         torch.nn.init.xavier_normal_(m.weight)
-        if m.bias.data is not None:
-          init.constant_(m.bias, 0) 
+        if m.bias is not None:
+          init.constant_(m.bias, 0)
 
   def forward(self, x, temperature=1.0, hard=0):
     x = x.view(x.size(0), -1)
     out_inf = self.inference(x, temperature, hard)
     z, y = out_inf['gaussian'], out_inf['categorical']
     out_gen = self.generative(z, y)
-    
+
     # merge output
     output = out_inf
     for key, value in out_gen.items():
